1272dfe8628710fcd83a7cd1b97840f69093ddab
Shirin Meggendorfer <shirinmeggendorfer@MBP-de-Shirin.fritz.box>
2024-05-30 15:20:19 +0200

    db update

--- e-commerce-backend/new_dump_db.sql
+++ e-commerce-backend/new_dump_db.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.11 (Homebrew)
--- Dumped by pg_dump version 14.11 (Homebrew)
+-- Dumped from database version 13.15 (Debian 13.15-1.pgdg120+1)
+-- Dumped by pg_dump version 16.3
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,6 +16,15 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO postgres;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -25,30 +34,176 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    id integer NOT NULL,
+    name character varying(255) NOT NULL
 );
+
+
+ALTER TABLE public.categories OWNER TO postgres;
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.categories_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.categories_id_seq OWNER TO postgres;
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.categories_id_seq OWNED BY public.categories.id;
+
 
 --
 -- Name: collections; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.collections (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    id integer NOT NULL,
+    name character varying(255) NOT NULL
 );
 
 
+ALTER TABLE public.collections OWNER TO postgres;
+
 --
--- Name: collections; Type: TABLE; Schema: public; Owner: postgres
+-- Name: collections_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.collections_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.collections_id_seq OWNER TO postgres;
+
+--
+-- Name: collections_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.collections_id_seq OWNED BY public.collections.id;
+
+
+--
+-- Name: coupons; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.coupons (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    amount NUMERIC,
-    available BOOLEAN DEFAULT FALSE
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    amount numeric,
+    available boolean DEFAULT false
 );
+
+
+ALTER TABLE public.coupons OWNER TO postgres;
+
+--
+-- Name: coupons_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.coupons_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.coupons_id_seq OWNER TO postgres;
+
+--
+-- Name: coupons_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.coupons_id_seq OWNED BY public.coupons.id;
+
+
+--
+-- Name: order_items; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.order_items (
+    id integer NOT NULL,
+    order_id integer,
+    product_id integer,
+    quantity integer,
+    price numeric
+);
+
+
+ALTER TABLE public.order_items OWNER TO postgres;
+
+--
+-- Name: order_items_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.order_items_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.order_items_id_seq OWNER TO postgres;
+
+--
+-- Name: order_items_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.order_items_id_seq OWNED BY public.order_items.id;
+
+
+--
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id integer NOT NULL,
+    user_id integer,
+    total_amount numeric,
+    status character varying(50)
+);
+
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.orders_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.orders_id_seq OWNER TO postgres;
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
 
 
 --
@@ -56,102 +211,403 @@ CREATE TABLE public.coupons (
 --
 
 CREATE TABLE public.products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    image VARCHAR(255),
-    category_id INTEGER REFERENCES public.categories(id),
-    collection_id INTEGER REFERENCES public.collections(id),
-    new_price NUMERIC,
-    old_price NUMERIC,
-    date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    available BOOLEAN DEFAULT TRUE,
-    "createdAt" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    id integer NOT NULL,
+    name character varying(255),
+    image character varying(255),
+    category_id integer,
+    collection_id integer,
+    new_price numeric,
+    old_price numeric,
+    date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    available boolean DEFAULT true,
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
+
+
+ALTER TABLE public.products OWNER TO postgres;
+
+--
+-- Name: products_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.products_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.products_id_seq OWNER TO postgres;
+
+--
+-- Name: products_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.products_id_seq OWNED BY public.products.id;
+
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255),
-    cart_data JSONB,
-    date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    is_admin BOOLEAN DEFAULT FALSE,
-    "createdAt" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    id integer NOT NULL,
+    name character varying(255),
+    email character varying(255),
+    password character varying(255),
+    cart_data jsonb,
+    date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    is_admin boolean DEFAULT false,
+    "createdAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 
-CREATE TABLE public.orders (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES public.users(id),
-    total_amount NUMERIC,
-    status VARCHAR(50)
-);
+ALTER TABLE public.users OWNER TO postgres;
 
-CREATE TABLE public.order_items (
-    id SERIAL PRIMARY KEY,
-    order_id INTEGER REFERENCES public.orders(id),
-    product_id INTEGER REFERENCES public.products(id),
-    quantity INTEGER,
-    price NUMERIC
-);
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: categories id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories ALTER COLUMN id SET DEFAULT nextval('public.categories_id_seq'::regclass);
+
+
+--
+-- Name: collections id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.collections ALTER COLUMN id SET DEFAULT nextval('public.collections_id_seq'::regclass);
+
+
+--
+-- Name: coupons id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coupons ALTER COLUMN id SET DEFAULT nextval('public.coupons_id_seq'::regclass);
+
+
+--
+-- Name: order_items id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items ALTER COLUMN id SET DEFAULT nextval('public.order_items_id_seq'::regclass);
+
+
+--
+-- Name: orders id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Name: products id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products ALTER COLUMN id SET DEFAULT nextval('public.products_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
 
 --
 -- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.categories (name) VALUES ('men'), ('women'), ('kids');
+COPY public.categories (id, name) FROM stdin;
+1	men
+2	women
+3	kids
+\.
+
 
 --
 -- Data for Name: collections; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.collections (name) VALUES ('womenCollection1'), ('menCollection1');
+COPY public.collections (id, name) FROM stdin;
+1	womenCollection1
+2	menCollection1
+\.
 
 
 --
--- Data for Name: Coupons; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: coupons; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.coupons (name, amount, available) VALUES ('testcoupon20',0.2,TRUE);
+COPY public.coupons (id, name, amount, available) FROM stdin;
+1	testcoupon20	0.2	t
+\.
 
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.users (name, email, password, date, is_admin) VALUES
-('TestUser', 'test@test.com', 'test1234', '2024-05-06 15:29:57.132183', false),
-('admin', 'admin@admin.com', 'admin', '2024-05-06 15:29:57.13414', true),
-('beispiel', 'beispiel3@beispiel.com', 'beispiel', '2024-05-06 15:29:57.13414', true),
-('beispiel4', 'beispiel4@beispiel.com', 'beispiel', '2024-05-06 15:29:57.13414', true),
-('beispiel', 'beispiel@beispiel.com', 'beispiel', '2024-05-06 15:29:57.13414', true);
-
---
--- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
---
--- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.products (name, image, category_id, collection_id, new_price, old_price, date, available) VALUES
-('Beispielprodukt 1', '/images/dress.png', 2, 1, 49.99, 59.99, '2024-05-06 15:31:03.660153', true),
-('Beispielprodukt 2', '/images/shirt.png', 1, 2, 29.99, 39.99, '2024-05-06 15:31:03.662069', true);
-
-
--- Assuming the first user (id = 1) is the user for the order
-INSERT INTO public.orders (user_id, total_amount, status) VALUES
-(2, 1, 'pending');
 
 --
 -- Data for Name: order_items; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
--- Assuming the first order (id = 1) and first product (id = 1)
-INSERT INTO public.order_items (order_id, product_id, quantity, price) VALUES
-(1, 1, 1, 49.99);
+COPY public.order_items (id, order_id, product_id, quantity, price) FROM stdin;
+1	1	1	1	49.99
+2	2	3	2	34
+3	3	3	1	34
+4	3	2	2	29.99
+5	4	1	1	49.99
+6	4	2	1	29.99
+7	5	1	4	49.99
+\.
+
+
+--
+-- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.orders (id, user_id, total_amount, status) FROM stdin;
+1	2	1	pending
+2	1	68.00	Pending
+3	1	93.98	Pending
+4	1	79.98	Pending
+5	1	199.96	Pending
+\.
+
+
+--
+-- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.products (id, name, image, category_id, collection_id, new_price, old_price, date, available, "createdAt", "updatedAt") FROM stdin;
+2	Beispielprodukt 2	/images/product_1717065498082.png	1	2	29.99	39.99	2024-05-06 15:31:03.662069	t	2024-05-27 07:57:34.518516	2024-05-30 10:38:18.094
+1	Beispielprodukt 1	/images/product_1717065508849.png	2	1	49.99	59.99	2024-05-06 15:31:03.660153	t	2024-05-27 07:57:34.518516	2024-05-30 10:38:28.866
+3	test women777	/images/product_1717065519868.png	2	1	34	22	2024-05-27 11:37:36.166954	t	2024-05-27 11:37:36.12	2024-05-30 10:38:39.884
+5	kids	/images/product_1717065527768.png	3	2	25	88	2024-05-27 14:35:39.81254	t	2024-05-27 14:35:39.8	2024-05-30 10:38:47.778
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name, email, password, cart_data, date, is_admin, "createdAt", "updatedAt") FROM stdin;
+2	admin	admin@admin.com	admin	\N	2024-05-06 15:29:57.13414	t	2024-05-27 07:57:34.51772	2024-05-27 07:57:34.51772
+3	beispiel	beispiel3@beispiel.com	beispiel	\N	2024-05-06 15:29:57.13414	t	2024-05-27 07:57:34.51772	2024-05-27 07:57:34.51772
+4	beispiel4	beispiel4@beispiel.com	beispiel	\N	2024-05-06 15:29:57.13414	t	2024-05-27 07:57:34.51772	2024-05-27 07:57:34.51772
+5	beispiel	beispiel@beispiel.com	beispiel	\N	2024-05-06 15:29:57.13414	t	2024-05-27 07:57:34.51772	2024-05-27 07:57:34.51772
+1	TestUser	test@test.com	test1234	{}	2024-05-06 15:29:57.132183	f	2024-05-27 07:57:34.51772	2024-05-30 10:39:12.676
+\.
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.categories_id_seq', 4, true);
+
+
+--
+-- Name: collections_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.collections_id_seq', 3, true);
+
+
+--
+-- Name: coupons_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.coupons_id_seq', 2, true);
+
+
+--
+-- Name: order_items_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.order_items_id_seq', 7, true);
+
+
+--
+-- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.orders_id_seq', 5, true);
+
+
+--
+-- Name: products_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.products_id_seq', 5, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 5, true);
+
+
+--
+-- Name: categories categories_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_name_key UNIQUE (name);
+
+
+--
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: collections collections_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.collections
+    ADD CONSTRAINT collections_name_key UNIQUE (name);
+
+
+--
+-- Name: collections collections_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.collections
+    ADD CONSTRAINT collections_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: coupons coupons_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coupons
+    ADD CONSTRAINT coupons_name_key UNIQUE (name);
+
+
+--
+-- Name: coupons coupons_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.coupons
+    ADD CONSTRAINT coupons_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: order_items order_items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: order_items order_items_order_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_order_id_fkey FOREIGN KEY (order_id) REFERENCES public.orders(id);
+
+
+--
+-- Name: order_items order_items_product_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.products(id);
+
+
+--
+-- Name: orders orders_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- Name: products products_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_category_id_fkey FOREIGN KEY (category_id) REFERENCES public.categories(id);
+
+
+--
+-- Name: products products_collection_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_collection_id_fkey FOREIGN KEY (collection_id) REFERENCES public.collections(id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
